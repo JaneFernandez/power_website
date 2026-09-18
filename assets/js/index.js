2142b939505171b2f7b7afa832f1a59f387faccc
Jane Fernandez <janefernax@gmail.com>
2026-09-18 13:32:32 +0200

    Ajouter bloc un
    Please enter the commit message for your changes. Lines starting

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// index.js - logique du quiz d'objectif (page index.html)
+
+function choisir(name, id) {
+    document.getElementById("hidden-goal-id").value = id;
+    document.getElementById("formulaire-email").style.display = "block";
+}
